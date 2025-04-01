04088da5e06f7b250cf796710db314f2615bd965
heutinkict-nkrikken <151822964+heutinkict-nkrikken@users.noreply.github.com>
2025-04-01 15:16:16 +0200

    / toegeveogd

--- hulpdesk-dashboard-widget.php
+++ hulpdesk-dashboard-widget.php
@@ -32,7 +32,7 @@ function hulpdesk_widget_content() {
     echo 'Heb je hulp nodig bij het beheren van je website, neem dan een kijkje in onze online kennisbank. Hier vind je antwoord op de meest gestelde vragen. Kom je er niet uit, dan kun je contact opnemen met onze servicedesk.</p>';
     echo '<p><strong>Ondersteuning</strong><br/ >';
     echo 'Heb je vragen over je website waar je met behulp van onze handleidingen niet uitkomt? Dan kun je contact opnemen met onze servicedesk op nummer <strong>074 240 46 66</strong> of mailen naar <a href="mailto:schoolwebsite@cloudwise.nl">schoolwebsite@cloudwise.nl</a>';
-    echo '<p>Je kunt ook een melding indienen via de Online Servicedesk. Meer informatie over hoe je ons kunt bereiken vind je op <a href="https://www.cloudwise.nl/service" target="_blank">www.cloudwise.nl/service</a></p>';
+    echo '<p>Je kunt ook een melding indienen via de Online Servicedesk. Meer informatie over hoe je ons kunt bereiken vind je op <a href="https://www.cloudwise.nl/service/" target="_blank">www.cloudwise.nl/service</a></p>';
 }
 
 // Voeg een versiecontrole toe via de GitHub API
